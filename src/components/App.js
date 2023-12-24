57a54b5b52b49ee820886e95de565ab990ebddbd
Rohit-del-source <rohitverma18158920@gmail.com>
2023-12-24 11:18:06 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -12,12 +12,38 @@ class App extends Component {
     };
   }
 
-  render() {
-    
-    const { index, slides } = this.state;
+  handleNext =() => {
+    const {index} = this.state;
+    const {slides} = this.props;
+    if(index < slides.length -1){
+      this.setState({index : index+1});
+    }
+  };
+  handlePrev= ()=> {
+    const{index} = this.state;
+    if(index > 0) {
+      this.setState({index: index -1});
+    }
+  };
 
+  handleRestart = () => {
+    this.setState({index: 0});
+  };
+
+  render() {
+   
+    const { index, slides } = this.state;
+    const currentSlide = slides[index];
     return (
-      <></>
+      <> 
+          <div>
+          <h1 id="title">{index.title}</h1>
+          <p id="text">{currentSlide.text}</p>
+          <button id="button-restart" onClick={this.handleRestart} disabled ={index ===0}>Restart</button>
+          <button id="button-prev" onClick={this.handlePrev} disabled={index === 0}>Prev</button>
+          <button id="button-next" onClick={this.handleNext} disabled={index === slides.length-1}>Next</button>
+          </div>
+      </>
     );
   }
 }
